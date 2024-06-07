b6f01d57834d664b582be88c58898efe501e8cbe
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-07 16:03:06 -0500

    removed extra usings in card controller

--- controllers/CardController.cs
+++ controllers/CardController.cs
@@ -1,12 +1,6 @@
-using System.Security.Claims;
-using System.Text;
 using BlastDeck.Data;
-using BlastDeck.Models;
 using BlastDeck.Models.DTOs;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
