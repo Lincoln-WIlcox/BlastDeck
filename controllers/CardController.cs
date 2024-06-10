ebb490ca186153a2ab176b5121fb1abf68c1a14c
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-10 14:24:50 -0500

    created endpoint for starring a card

--- controllers/CardController.cs
+++ controllers/CardController.cs
@@ -1,4 +1,6 @@
+using System.Security.Claims;
 using BlastDeck.Data;
+using BlastDeck.Models;
 using BlastDeck.Models.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -24,5 +26,29 @@ public class CardController : ControllerBase
         return Ok(
             _dbContext.Cards.Include(c => c.Answers).Select(c => new GetCardsDTO(c)).ToList()
         );
+    }
+
+    [HttpPost("{id}/star")]
+    [Authorize]
+    public IActionResult StarCard(int id)
+    {
+        Card? card = _dbContext.Cards.SingleOrDefault(c => c.Id == id);
+
+        if (card == null)
+        {
+            return BadRequest();
+        }
+
+        var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var profile = _dbContext.UserProfiles.SingleOrDefault(up =>
+            up.IdentityUserId == identityUserId
+        );
+
+        UserCard userCard = new UserCard { CardId = card.Id, UserId = profile.Id };
+
+        _dbContext.UserCards.Add(userCard);
+        _dbContext.SaveChanges();
+
+        return NoContent();
     }
 }
