cc0889e985ea4eb4a60ac28d97e5d419e60185c1
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-11 15:07:03 -0500

    edit card page gets card  id

--- client/src/pages/EditCard.jsx
+++ client/src/pages/EditCard.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react"
 import CardForm from "../components/cardform/CardForm"
 import { createCardByMe } from "../managers/cardManager"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 const EditCard = () =>
 {
+    const [existingCard, setExistingCard] = useState({})
+    const { cardId } = useParams()
     const navigate = useNavigate()
+
+    useEffect(
+        () =>
+        {
+            
+        }, [cardId]
+    )
+
     const handleCardSubmitted = (card) =>
     {
         createCardByMe(card).then(
