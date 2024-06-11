2b6c3bb1bd601ddc074b26da06560703a7ad7c84
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-11 11:20:17 -0500

    create card page adds creator id to card

--- client/src/pages/CreateCard.jsx
+++ client/src/pages/CreateCard.jsx
@@ -1,8 +1,17 @@
 import CardForm from "../components/cardform/CardForm"
+import { UserContext } from "../App"
+import { useContext } from "react"
 
 const CreateCard = () =>
 {
-    return <CardForm />
+    const user = useContext(UserContext)
+
+    const handleCardSubmitted = (card) =>
+    {
+        card.creatorId = user.id
+    }
+
+    return <CardForm onCardSubmitted={handleCardSubmitted} />
 }
 
 export default CreateCard
