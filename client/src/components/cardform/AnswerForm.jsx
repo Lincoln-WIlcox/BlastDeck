8b7f5265150bc2a7999b0d6e333e2c5ab8c9d719
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-11 10:42:45 -0500

    user can mark correct answer

--- client/src/components/cardform/AnswerForm.jsx
+++ client/src/components/cardform/AnswerForm.jsx
@@ -1,13 +1,28 @@
-import { Button, Input, InputGroup } from "reactstrap"
+import { Button, Input, InputGroup, Label } from "reactstrap"
 
-const AnswerForm = ({ answer, answerChanged, onRemovePressed }) =>
+const AnswerForm = ({ answer, answerChanged, onRemovePressed, isCorrectAnswer, onCorrectAnswerSelected }) =>
 {
     const handleAnswerChange = (e) =>
     {
         answerChanged(e.target.value)
     }
 
+    const handleCorrectAnswerSelect = () =>
+    {
+        onCorrectAnswerSelected()
+    }
+
     return <div className="d-flex">
+        <div className="d-flex gap-2 m-2 align-items-center">
+            <Label className="my-text smaller-text text-nowrap">Mark Correct</Label>
+            <Input
+                type="radio"
+                name="correct-answer"
+                checked={isCorrectAnswer}
+                onChange={handleCorrectAnswerSelect}
+            />
+        </div>
+
         <InputGroup>
             <Button onClick={onRemovePressed}>Remove</Button>
             <Input type="text" value={answer} onChange={handleAnswerChange} />
--- client/src/components/cardform/CardForm.jsx
+++ client/src/components/cardform/CardForm.jsx
@@ -5,6 +5,7 @@ import AnswerForm from "./AnswerForm"
 const CardForm = () =>
 {
     const [answers, setAnswers] = useState([])
+    const [correctAnswerIndex, setCorrectAnswerIndex] = useState(-1)
 
     const handleAddAnswerPress = () =>
     {
@@ -20,28 +21,42 @@ const CardForm = () =>
 
     const handleAnswerRemove = (index) =>
     {
+        if (index == correctAnswerIndex)
+        {
+            setCorrectAnswerIndex(-1)
+        }
+            
         setAnswers(answers.filter((a, i) => i != index))
     }
 
     return <div className="d-flex flex-column gap-3">
-        <Row>
-            <InputGroup>
-                <InputGroupText>
-                    Image
-                </InputGroupText>
-                <Input placeholder="URL" />
-            </InputGroup>
-        </Row>
-        <Row className="mx-5">
-            <Button onClick={handleAddAnswerPress}>Add Answer</Button>
-        </Row>
-        {
-            answers.map((a, i) =>
-                <Row key={i + "a"}>
-                    <AnswerForm answer={a} answerChanged={(newAnswer) => handleAnswerChange(newAnswer, i)} onRemovePressed={() => handleAnswerRemove(i)} />
-                </Row>
-            )
-        }
+        <div className="d-flex flex-column gap-3 m-2">
+            <Row>
+                <InputGroup>
+                    <InputGroupText>
+                        Image
+                    </InputGroupText>
+                    <Input placeholder="URL" />
+                </InputGroup>
+            </Row>
+            <Row className="mx-5">
+                <Button onClick={handleAddAnswerPress}>Add Answer</Button>
+            </Row>
+        </div>
+        <div className="d-flex flex-column gap-3">
+            {
+                answers.map((a, i) =>
+                    <Row key={i + "a"}>
+                        <AnswerForm
+                            answer={a}
+                            answerChanged={(newAnswer) => handleAnswerChange(newAnswer, i)}
+                            onRemovePressed={() => handleAnswerRemove(i)}
+                            isCorrectAnswer={correctAnswerIndex == i}
+                            onCorrectAnswerSelected={() => setCorrectAnswerIndex(i)} />
+                    </Row>
+                )
+            }
+        </div>
     </div>
 }
 
