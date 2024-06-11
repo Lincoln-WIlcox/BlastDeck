5272c246b092405bac62e4c0510801feca199c57
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-11 09:54:23 -0500

    added text input and add answer button to card form

--- client/src/components/card/CardForm.jsx
+++ client/src/components/card/CardForm.jsx
@@ -1,7 +1,20 @@
+import { Button, Input, InputGroup, InputGroupText, Row } from "reactstrap"
 
 const CardForm = () =>
 {
-    return <> card form </>
+    return <div className="d-flex flex-column gap-3">
+        <Row>
+            <InputGroup>
+                <InputGroupText>
+                    Image
+                </InputGroupText>
+                <Input placeholder="URL" />
+            </InputGroup>
+        </Row>
+        <Row className="mx-5">
+            <Button>Add Answer</Button>
+        </Row>
+    </div>
 }
 
 export default CardForm
