e8b3bf686c730d1915adfbcf487652e929727d43
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-12 14:59:24 -0500

    added input field for filter

--- client/src/pages/AllCards.jsx
+++ client/src/pages/AllCards.jsx
@@ -5,6 +5,7 @@ import { getCards } from "../managers/cardManager"
 const AllCards = () =>
 {
     const [allCards, setAllCards] = useState([])
+    const [searchQuery, setSearchQuery] = useState("")
 
     const fetchAndSetAllCards = () =>
     {
@@ -18,7 +19,10 @@ const AllCards = () =>
         }, []
     )
 
-    return <CardList cards={allCards} cardsUpdated={fetchAndSetAllCards} />
+    return <>
+        <input type="search" placeholder="filter by english word" className="text-black rounded my-shadow-light p-1" onChange={(e) => setSearchQuery(e.target.value)} value={searchQuery} />
+        <CardList cards={allCards} cardsUpdated={fetchAndSetAllCards} />
+    </>
 }
 
 export default AllCards
