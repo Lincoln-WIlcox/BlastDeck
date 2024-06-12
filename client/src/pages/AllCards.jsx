bca7de06781ca10b3c00b745f1d031d7f5f55d19
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-12 15:03:52 -0500

    added new state to represent the cards being shown, when search query is changed shown cards are filtered

--- client/src/pages/AllCards.jsx
+++ client/src/pages/AllCards.jsx
@@ -5,6 +5,7 @@ import { getCards } from "../managers/cardManager"
 const AllCards = () =>
 {
     const [allCards, setAllCards] = useState([])
+    const [showCards, setShowCards] = useState([])
     const [searchQuery, setSearchQuery] = useState("")
 
     const fetchAndSetAllCards = () =>
@@ -19,9 +20,23 @@ const AllCards = () =>
         }, []
     )
 
+    useEffect(
+        () =>
+        {
+            if(searchQuery)
+            {
+                setShowCards(allCards.filter(c => c.englishWord.toLowerCase().includes(searchQuery.toLowerCase())))
+            } else
+            {
+                setShowCards(allCards)
+            }
+
+        }, [searchQuery, allCards]
+    )
+
     return <>
         <input type="search" placeholder="filter by english word" className="text-black rounded my-shadow-light p-1" onChange={(e) => setSearchQuery(e.target.value)} value={searchQuery} />
-        <CardList cards={allCards} cardsUpdated={fetchAndSetAllCards} />
+        <CardList cards={showCards} cardsUpdated={fetchAndSetAllCards} />
     </>
 }
 
