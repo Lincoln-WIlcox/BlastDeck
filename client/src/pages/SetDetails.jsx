cbfc812fd1b7e22abd236b9f1615cfb68b6c9658
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-13 11:13:37 -0500

    set details page gets set id

--- client/src/pages/SetDetails.jsx
+++ client/src/pages/SetDetails.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 const SetDetails = () =>
 {
+    const {setId} = useParams()
+    const [set, setSet] = useState({})
+
+    useEffect(
+        () =>
+        {
+            
+        }, [setId]
+    )
+
     return <> set details </>
 }
 
