b1cad8670ff63a2f950152bd97b6d836bae81533
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-13 10:18:14 -0500

    all sets page gets all sets

--- client/src/pages/AllSets.jsx
+++ client/src/pages/AllSets.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react"
 import SetList from "../components/set/SetList"
+import { getSets } from "../managers/setManager"
 
 const AllSets = () =>
 {
+    const [allSets, setAllSets] = useState([])
+
+    useEffect(
+        () =>
+        {
+            getSets().then(setAllSets)
+        }, []
+    )
+
     return <SetList />
 }
 
