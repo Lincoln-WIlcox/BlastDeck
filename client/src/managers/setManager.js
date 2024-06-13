33288b71759d460d3da696f98ffaa69635fae668
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-13 11:34:31 -0500

    set details page gets set

--- client/src/managers/setManager.js
+++ client/src/managers/setManager.js
@@ -2,3 +2,8 @@ export const getSets = () =>
 {
     return fetch('/api/set').then(res => res.json())
 }
+
+export const getSetById = (id) =>
+{
+    return fetch(`/api/set/${id}`).then(res => res.json())
+}
--- client/src/pages/SetDetails.jsx
+++ client/src/pages/SetDetails.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { getSetById } from "../managers/setManager"
 
 const SetDetails = () =>
 {
-    const {setId} = useParams()
+    const { setId } = useParams()
     const [set, setSet] = useState({})
 
     useEffect(
         () =>
         {
-            
+            if(setId)
+            {
+                getSetById(setId).then(setSet)
+            }
         }, [setId]
     )
 
