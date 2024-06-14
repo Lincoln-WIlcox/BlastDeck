797fdb0091462ca3f492238d88bd9c920c12e0bd
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-14 14:46:36 -0500

    added pick mini card component

---
+++ client/src/components/card/mini-card/PickMiniCard.jsx
@@ -0,0 +1,48 @@
+import MiniCard from "./MiniCard"
+import MiniCardEditButton from "./MiniCardEditButton"
+import MiniCardDeleteButton from "./MiniCardDeleteButton"
+import MiniCardSidebar from "./MiniCardSidebar"
+import MiniCardImage from "./MiniCardImage"
+import MiniCardBody from "./MiniCardBody"
+import MiniCardHeader from "./MiniCardHeader"
+import MiniCardHeaderText from "./MiniCardHeaderText"
+import MiniCardStarButton from "./MiniCardStarButton"
+import MiniCardSubheader from "./MiniCardSubheader"
+import MiniCardContent from "./MiniCardContent"
+
+const PickMiniCard = ({ card, onStarButtonPress, onUnstarButtonPress }) =>
+{
+    return <MiniCard card={card}>
+        <MiniCardSidebar>
+            <Input type="radio" />
+            <MiniCardImage src={card?.imageURL} />
+        </MiniCardSidebar>
+
+        <MiniCardBody>
+            <MiniCardHeader>
+                <MiniCardHeaderText>{card?.englishWord}</MiniCardHeaderText>
+                <MiniCardHeaderText>{card?.correctAnswer.word}</MiniCardHeaderText>
+                <MiniCardStarButton starred={card?.starred} onStarButtonPress={onStarButtonPress} onUnstarButtonPress={onUnstarButtonPress} />
+            </MiniCardHeader>
+            <MiniCardSubheader>
+                {
+                    card?.answers.map(a =>
+                        a.word != card?.correctAnswer.word &&
+                        <p key={a.id + "a"} className="my-text smaller-text">
+                            {a.word}
+                        </p>
+                    )
+                }
+            </MiniCardSubheader>
+            {/* <CardContext.Provider value={card}>
+                        <Row>
+                            {
+                                children && children
+                            }
+                        </Row>
+                    </CardContext.Provider> */}
+        </MiniCardBody>
+    </MiniCard>
+}
+
+export default PickMiniCard
