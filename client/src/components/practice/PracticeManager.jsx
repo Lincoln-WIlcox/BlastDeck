322eef5bc64b509c1ff7b5df5acfbae26b379fdc
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-17 13:45:52 -0500

    practice manager navigates to practice after association stage is complete

--- client/src/components/practice/PracticeManager.jsx
+++ client/src/components/practice/PracticeManager.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react"
 import PracticeCardAssociation from "./PracticeCardAssociation"
+import { useNavigate } from "react-router-dom"
 
 const PracticeManager = ({ cards }) =>
 {
     const [currentCardIndex, setCurrentCardIndex] = useState(0)
 
-    return <PracticeCardAssociation card={cards[currentCardIndex]} />
+    const navigate = useNavigate()
+
+    const handleContinuePressed = () =>
+    {
+        if(currentCardIndex == cards.length - 1)
+        {
+            navigate("/practice")
+        }
+        setCurrentCardIndex(currentCardIndex + 1)
+    }
+
+    return <PracticeCardAssociation card={cards[currentCardIndex]} onContinuePressed={handleContinuePressed} />
 }
 
 export default PracticeManager
