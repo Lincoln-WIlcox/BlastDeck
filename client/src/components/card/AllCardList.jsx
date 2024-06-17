5a40a415170327a0aa67be717bd2b4ec6e2a3d7b
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-17 09:54:16 -0500

    removed add star button option from all cards list

--- client/src/components/card/AllCardList.jsx
+++ client/src/components/card/AllCardList.jsx
@@ -3,11 +3,11 @@ import MiniCard from "./mini-card/MiniCard"
 import MyMiniCard from "./mini-card/MyMiniCard"
 import AllMiniCard from "./mini-card/AllMiniCard"
 
-const AllCardsList = ({ addStarButton, cards, cardsUpdated }) =>
+const AllCardsList = ({ cards, cardsUpdated }) =>
 {
     return (
         cards.map(c =>
-            <AllMiniCard key={c.id + "c"} card={c} cardsUpdated={cardsUpdated} addStarButton={addStarButton} />)
+            <AllMiniCard key={c.id + "c"} card={c} cardsUpdated={cardsUpdated} />)
     )
 }
 
