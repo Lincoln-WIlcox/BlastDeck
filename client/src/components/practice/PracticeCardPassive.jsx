8e1d25ce073d331fd75fa9a3c2874dcd0cb6d982
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-17 15:31:42 -0500

    shows feedback for if you got answer correct and allows continuing to next card

--- client/src/components/practice/PracticeCardPassive.jsx
+++ client/src/components/practice/PracticeCardPassive.jsx
@@ -1,36 +1,60 @@
 import { Button } from "reactstrap"
 import "./Practice.css"
 import { answerCard } from "../../managers/answerManager"
+import { useEffect, useState } from "react"
 
 const PracticeCardPassive = ({ card, onContinuePressed }) =>
 {
+    const [answeredCorrectly, setAnsweredCorrectly] = useState()
+
     const handleAnswerChosen = (answerId) =>
     {
         //the endpoint will return if the answer is correct. The endpoint will also make a userAnswer
-        answerCard(card.id, answerId).then(
-            (correct) =>
-            {
-                if(correct)
-                {
-                    console.log("correct!")
-                } else
-                {
-                    console.log("incorrect")
-                }
-            }
-        )
+        answerCard(card.id, answerId).then(setAnsweredCorrectly)
+    }
+
+    useEffect(
+        () =>
+        {
+            setAnsweredCorrectly(undefined)
+        }, [card]
+    )
+
+    let content
+
+    switch(answeredCorrectly)
+    {
+        case undefined:
+            content = card?.answers.map(a =>
+                <div>
+                    <Button className="my-text" onClick={() => handleAnswerChosen(a.id)}>{a.word}</Button>
+                </div>)
+            break
+        case true:
+            content = <div>
+                <p className="my-text text-center">Correct!</p>
+            </div>
+            break
+        case false:
+            content = <div>
+                <p className="my-text text-center">you answered wrong... bruh...</p>
+            </div>
+            break
     }
 
     return <div className="d-flex flex-column justify-content-between practice-container">
-        <div className="d-flex flex-column gap-3">
+        <div>
             <img className="practice-image" src={card?.imageURL} />
         </div>
         <div className="d-flex flex-column">
             {
-                card?.answers.map(a =>
-                    <div>
-                        <Button className="my-text" onClick={() => handleAnswerChosen(a.id)}>{a.word}</Button>
-                    </div>)
+                content
+            }
+            {
+                answeredCorrectly != undefined &&
+                <div>
+                    <Button className="my-text" onClick={onContinuePressed}>Continue</Button>
+                </div>
             }
         </div>
     </div>
