2117abcb658dba768e66566d7758e068578dab95
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-17 10:19:40 -0500

    added submit button

--- client/src/pages/AddCardToSet.jsx
+++ client/src/pages/AddCardToSet.jsx
@@ -3,21 +3,29 @@ import { useParams } from "react-router-dom"
 import { getSetById } from "../managers/setManager"
 import PickMiniCardList from "../components/card/PickMiniCardList"
 import { getStarred } from "../managers/cardManager"
+import { Button } from "reactstrap"
 
 const AddCardToSet = () =>
 {
+    const [set, setSet] = useState({})
     const [cards, setCards] = useState([])
     const [selectedCards, setSelectedCards] = useState([])
     const { setId } = useParams()
 
-    const fetchAndSetSet = () =>
+    const fetchAndSetStarredCards = () =>
     {
         getStarred().then(setCards)
+    }
+
+    const fetchAndSetSet = () =>
+    {
+        getSetById(setId).then(setSet)
     }
 
     useEffect(
         () =>
         {
+            fetchAndSetStarredCards()
             fetchAndSetSet()
         }, [setId]
     )
@@ -33,7 +41,16 @@ const AddCardToSet = () =>
         }
     }
 
-    return <PickMiniCardList cards={cards} cardsUpdated={fetchAndSetSet} onCheckboxChanged={handleCheckboxChanged} />
+    const handleAddCardsButtonPressed = () =>
+    {
+        
+    }
+
+    return <div>
+        <Button onClick={handleAddCardsButtonPressed}>Add Cards To {set.setName}</Button>
+        <PickMiniCardList cards={cards} cardsUpdated={fetchAndSetStarredCards} onCheckboxChanged={handleCheckboxChanged} />
+    </div>
+
 }
 
 export default AddCardToSet
