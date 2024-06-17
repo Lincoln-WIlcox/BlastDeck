70e8388a176d2107fe366bf706456df8d0e28942
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-17 10:55:24 -0500

    added practice all button

--- client/src/pages/PracticePage.jsx
+++ client/src/pages/PracticePage.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom"
+
+import { Button } from "reactstrap"
 
 const PracticePage = () =>
 {
-    return <>practice page</>
+    return <div>
+        <Link to="/practice/all">
+            <Button>Practice All</Button>
+        </Link>
+    </div>
 }
 
 export default PracticePage
