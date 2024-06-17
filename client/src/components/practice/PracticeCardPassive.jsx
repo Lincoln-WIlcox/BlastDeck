dc305dd2839c22cbc068f1aef61ad0558fd7d08b
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-06-17 14:28:30 -0500

    created practice passive stage component

---
+++ client/src/components/practice/PracticeCardPassive.jsx
@@ -0,0 +1,18 @@
+import { Button } from "reactstrap"
+import "./Practice.css"
+
+const PracticeCardPassive = ({ card, onContinuePressed }) =>
+{
+    return <div className="d-flex flex-column justify-content-between practice-container">
+        <div className="d-flex flex-column gap-3">
+            <img className="practice-image" src={card?.imageURL} />
+            <p className="my-text text-center">{card?.correctAnswer.word}</p>
+            <p className="my-text text-center">{card?.englishWord}</p>
+        </div>
+        <div>
+            <Button className="my-text" onClick={onContinuePressed}>Continue</Button>
+        </div>
+    </div>
+}
+
+export default PracticeCardPassive
